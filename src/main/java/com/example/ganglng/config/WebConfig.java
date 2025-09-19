4a7f8d54f9c48c922e28babbfa06a4a499c99400
sssiion <rta2752775@gmail.com>
2025-09-19 16:38:27 +0900

    dd
    dd

--- src/main/java/com/example/ganglng/config/WebConfig.java
+++ src/main/java/com/example/ganglng/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig  implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // /api/ 로 시작하는 모든 요청에 대해
-                .allowedOrigins("http://localhost:3000") // http://localhost:3000 (React 개발서버)의 요청을 허용
+                .allowedOrigins("https://ganglngreact-production.up.railway.app") // http://localhost:3000 (React 개발서버)의 요청을 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowCredentials(true);
     }
